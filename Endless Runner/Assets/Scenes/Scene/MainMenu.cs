d952592129e43668256329ca43a88ba0e8dd0dd2
xenaapb <xena.apb@gmail.com>
2022-10-13 09:52:36 +0100

    game 0.1

---
+++ Endless Runner/Assets/Scenes/Scene/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+ //   public void PlayGame()
+  //  {
+    //   UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // All of these examples loads 'Level_0'
+  //  }
+
+//   public void QuitGame()
+  //  {
+   //     Application.Quit();
+  //  }
+}
